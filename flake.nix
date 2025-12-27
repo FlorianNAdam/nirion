2a3262c9e58e6a8fe643739076ed495467919c88
FlorianNAdam <64063379+FlorianNAdam@users.noreply.github.com>
2025-12-27 22:21:17 +0100

    add shell completions to nix package

--- flake.nix
+++ flake.nix
@@ -43,6 +43,18 @@
           postInstall = ''
             wrapProgram $out/bin/nirion \
               --prefix PATH : ${pkgs.skopeo}/bin
+
+             # Bash completion
+            mkdir -p $out/share/bash-completion/completions
+            COMPLETE=bash $out/bin/nirion > $out/share/bash-completion/completions/nirion
+
+            # Zsh completion
+            mkdir -p $out/share/zsh/site-functions
+            COMPLETE=zsh $out/bin/nirion > $out/share/zsh/site-functions/_nirion
+
+            # Fish completion
+            mkdir -p $out/share/fish/vendor_completions.d
+            COMPLETE=fish $out/bin/nirion > $out/share/fish/vendor_completions.d/nirion.fish
           '';
         };
       in
