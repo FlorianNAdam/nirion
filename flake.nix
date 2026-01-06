319f4294f524085650fbf48e7843071b80593f7c
FlorianNAdam <64063379+FlorianNAdam@users.noreply.github.com>
2026-01-06 11:41:56 +0100

    refactor arion config generation

--- flake.nix
+++ flake.nix
@@ -316,38 +316,71 @@
                 "${file}";
             };
 
-            virtualisation.arion.projects = lib.mapAttrs (
-              projectName: projectConfig:
+            virtualisation.arion.projects =
               let
-                services = projectConfig.settings.services or { };
-                resolvedServices = lib.mapAttrs (
-                  serviceName: serviceConfig:
-                  let
-                    attrName = lib.attrByPath [
-                      "service"
+                imageTransform =
+                  projectName: serviceName: serviceConfig:
+                  if builtins.hasAttr "image" serviceConfig then
+                    let
+                      lockedImages = nirionConfig.out.locked_images;
+                      lockedImage = lockedImages."${projectName}.${serviceName}" or null;
+                    in
+                    serviceConfig // { image = lockedImage; }
+                  else
+                    serviceConfig;
+
+                lockedImageTransform =
+                  projectName: serviceName: serviceConfig:
+                  if builtins.hasAttr "locked_image" serviceConfig then
+                    let
+                      lockedImages = nirionConfig.out.locked_images;
+                      lockedImage =
+                        lockedImages."${serviceConfig.locked_image}"
+                          or (throw "nirion: Image reference ${serviceConfig.locked_image} not found");
+                    in
+                    (builtins.removeAttrs serviceConfig [
                       "locked_image"
-                    ] "${projectName}.${serviceName}" serviceConfig;
-                  in
-                  serviceConfig
-                  // (
-                    if builtins.hasAttr attrName nirionConfig.out.locked_images then
-                      {
-                        service = (builtins.removeAttrs (serviceConfig.service or { }) [ "locked_image" ]) // {
-                          image = nirionConfig.out.locked_images.${attrName};
-                        };
+                    ])
+                    // {
+                      image = lockedImage;
+                    }
+                  else
+                    serviceConfig;
+
+                applyTransforms =
+                  transforms: projectName: serviceName: serviceConfig:
+                  lib.lists.foldl (
+                    svcConfig: transform: transform projectName serviceName svcConfig
+                  ) serviceConfig transforms;
+
+                transformService = applyTransforms [
+                  imageTransform
+                  lockedImageTransform
+                ];
+              in
+              lib.mapAttrs (
+                projectName: projectConfig:
+                let
+                  services = projectConfig.settings.services or { };
+                  transform = transformService projectName;
+                  transformedServices = lib.mapAttrs (
+                    serviceName: serviceConfig:
+                    if builtins.hasAttr "service" serviceConfig then
+                      serviceConfig
+                      // {
+                        service = transform serviceName serviceConfig.service;
                       }
                     else
-                      { }
-                  )
-                ) services;
-              in
-              projectConfig
-              // {
-                settings = (projectConfig.settings or { }) // {
-                  services = resolvedServices;
-                };
-              }
-            ) nirionConfig.projects;
+                      serviceConfig
+                  ) services;
+                in
+                projectConfig
+                // {
+                  settings = (projectConfig.settings or { }) // {
+                    services = transformedServices;
+                  };
+                }
+              ) nirionConfig.projects;
           };
         };
     };
