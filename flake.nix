4a7d6654964949b613388fab058883cd94b00d52
FlorianNAdam <64063379+FlorianNAdam@users.noreply.github.com>
2026-04-05 16:37:27 +0200

    add nirion.lib with perl healthcheck

--- flake.nix
+++ flake.nix
@@ -416,6 +416,55 @@
                   };
                 }
               ) nirionConfig.projects;
+
+            lib.nirion = {
+              mkPerlHealthcheck =
+                {
+                  binary ? "perl",
+                  host ? "localhost",
+                  port,
+                  path,
+                  result,
+                }:
+                let
+                  dockerEscape = string: builtins.replaceStrings [ "$" ] [ "$$" ] string;
+                  resultString = if builtins.isString result then result else builtins.toJSON result;
+                in
+                [
+                  "CMD"
+                  "${binary}"
+                  "-MIO::Socket::INET"
+                  "-e"
+                  (dockerEscape ''
+                    $s = IO::Socket::INET->new("${host}:${builtins.toString port}") or do {
+                      print "Failed to connect";
+                      exit 1;
+                    };
+                    print "Connected";
+                    print $s "GET ${path} HTTP/1.0\r\n";
+                    print $s "Host: ${host}\r\n";
+                    print $s "Connection: close\r\n";
+                    print $s "\r\n";
+                    $/ = undef;
+                    $resp = <$s>;
+                    $body = (split /\r?\n\r?\n/, $resp, 2)[1];
+
+                    my $expected = <<'JSON';
+                    ${resultString}
+                    JSON
+
+                    $body =~ s/^\s+//;
+                    $body =~ s/\s+$//;
+                    $expected =~ s/^\s+//;
+                    $expected =~ s/\s+$//;
+
+                    print "Healthcheck response: >>>$body<<<\n";
+                    print "Healthcheck expected: >>>$expected<<<\n";
+
+                    exit($body eq $expected ? 0 : 1);
+                  '')
+                ];
+            };
           };
         };
     };
